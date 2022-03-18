1411644fc9aee81b667418a37cf3e6bf3d6a555e
ginpei <ginpei@gmail.com>
2022-03-18 06:48:58 +0000

    refactor: 💡 use Tailwind CSS

--- src/ui/simples/HStack.tsx
+++ src/ui/simples/HStack.tsx
@@ -2,15 +2,9 @@ export interface HStackProps {
   children: React.ReactNode;
 }
 
-const style: React.CSSProperties = {
-  display: 'flex',
-  flexDirection: 'row',
-  gap: '1rem',
-}
-
 export function HStack({ children }: HStackProps): JSX.Element {
   return (
-    <div className="HStack" style={style}>
+    <div className="HStack flex flex-row gap-4">
       {children}
     </div>
   );
--- src/ui/simples/VStack.tsx
+++ src/ui/simples/VStack.tsx
@@ -2,15 +2,9 @@ export interface VStackProps {
   children: React.ReactNode;
 }
 
-const style: React.CSSProperties = {
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '1rem',
-}
-
 export function VStack({ children }: VStackProps): JSX.Element {
   return (
-    <div className="VStack" style={style}>
+    <div className="VStack flex flex-col gap-4">
       {children}
     </div>
   );
