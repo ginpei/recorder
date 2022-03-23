c6449c54c84c167e2874c1791952e3d379aca3de
ginpei <ginpei@gmail.com>
2022-03-23 23:43:36 +0000

    feat: 🎸 download

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-let recorder: MediaRecorder | null = null;
+let audioRecorder: MediaRecorder | null = null;
+let audioBlob = new Blob();
 
 export function main() {
   $('#record').onclick = onRecordClick;
@@ -6,34 +7,57 @@ export function main() {
 }
 
 async function onRecordClick() {
+  audioRecorder = await createRecorder();
+  audioRecorder.start();
+}
+
+async function onStopClick() {
+  audioRecorder?.stop();
+}
+
+async function createRecorder(): Promise<MediaRecorder> {
   const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
   const chunks: Blob[] = [];
 
-  recorder = new MediaRecorder(stream);
+  const recorder = new MediaRecorder(stream);
 
   recorder.ondataavailable = function ({ data }) {
     chunks.push(data);
   };
 
   recorder.onstop = () => {
-    onRecorderStop(chunks);
+    const blob = new Blob(chunks, { 'type': 'audio/ogg; codecs=opus' });
+    setAudioBlob(blob);
   };
 
-  recorder.start();
+  return recorder;
 }
 
-function onStopClick() {
-  recorder?.stop();
-}
-
-function onRecorderStop(chunks: Blob[]) {
-  const audioBlob = new Blob(chunks, { 'type': 'audio/ogg; codecs=opus' });
-
-  setPlayerUrl(URL.createObjectURL(audioBlob));
+function setAudioBlob(newAudioBlob: Blob) {
+  audioBlob = newAudioBlob;
+  const url = URL.createObjectURL(audioBlob);
+  setPlayerUrl(url);
+  addDownloadLink(url);
 }
 
 function setPlayerUrl(url: string) {
-  $<HTMLAudioElement>('#player').src = url;
+  const elPlayer = $<HTMLAudioElement>('#player');
+  elPlayer.src = url;
+}
+
+function addDownloadLink(url: string) {
+  const text = new Date().toISOString();
+
+  const el = document.createElement('a');
+  el.download = `${text}.oga`;
+  el.href = url;
+  el.textContent = text;
+
+  const li = document.createElement('li');
+  li.append(el);
+
+  const elList = $('#links');
+  elList.append(li);
 }
 
 function $<T extends HTMLElement>(selector: string, parent: Element | Document = document) {
