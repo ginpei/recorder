13b5727cf27e004d3e17b6ef3247ef411b119bc4
ginpei <ginpei@gmail.com>
2022-03-27 00:23:03 +0000

    refactor: 💡 types

--- src/lamejs.js
+++ src/lamejs.js
@@ -2,30 +2,31 @@
 
 /**
  * @typedef {{
- *   Mp3Encoder: {
- *     new (
- *       channels: number,
- *       sampleRate: number,
- *       kBps: number
- *     ): {
- *       encodeBuffer(center: ArrayLike<number>): Int8Array;
- *       encodeBuffer(left: ArrayLike<number>, right: ArrayLike<number>): Int8Array;
- *       flush(): Int8Array;
- *     };
- *   };
+ *   Mp3Encoder: Mp3Encoder;
  *   WavHeader: {
  *     readonly data: 1684108385;
  *     readonly fmt_: 1718449184;
  *     readonly RIFF: 1380533830;
  *     readonly WAVE: 1463899717;
- *     readHeader(dataView: DataView): {
- *       channels: number;
- *       dataLen: number;
- *       dataOffset: number;
- *       sampleRate: number;
- *     };
+ *     readHeader(dataView: DataView): WavHeaderInstance;
  *   };
  * }} LameJs
+ * @typedef {{
+ *   new (
+ *     channels: number,
+ *     sampleRate: number,
+ *     kBps: number
+ *   ): Mp3Encoder;
+ *   encodeBuffer(center: ArrayLike<number>): Int8Array;
+ *   encodeBuffer(left: ArrayLike<number>, right: ArrayLike<number>): Int8Array;
+ *   flush(): Int8Array;
+ * }} Mp3Encoder
+ * @typedef {{
+ *   channels: number;
+ *   dataLen: number;
+ *   dataOffset: number;
+ *   sampleRate: number;
+ * }} WavHeaderInstance
  */
 
 /** @type {LameJs} */
